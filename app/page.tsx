206d503818680e0b612d2bcf71125ef68fa72c0f
Filip Taifas <filip.taifas@payset.io>
2026-04-03 16:15:38 +0300

    Add view transitions to homepage hobbies
    - Add transitionTypes to Link for nav-forward animation
    - Add share='morph' to photo transitions
    - Add share='text-morph' to title transitions
    - Add default='none' to prevent unwanted animations

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,17 @@ export default function Page() {
   return (
     <div className="grid h-[100dvh] grid-rows-3 md:grid-cols-3 md:grid-rows-1">
       {(hobbies as Hobby[]).map((hobby) => (
-        <Link href={`/${hobby.id}`} className="relative" key={hobby.id}>
-          <ViewTransition name={`${hobby.id}-photo`}>
+        <Link
+          href={`/${hobby.id}`}
+          className="relative"
+          key={hobby.id}
+          transitionTypes={["nav-forward"]}
+        >
+          <ViewTransition
+            name={`${hobby.id}-photo`}
+            share="morph"
+            default="none"
+          >
             <div
               className={`absolute inset-0 bg-[${hobby.color}] bg-position-[center_top_1rem] bg-size-[40%] bg-no-repeat md:bg-cover lg:bg-contain`}
               style={{ backgroundImage: `url(/${hobby.id}.png)` }}
@@ -17,7 +26,11 @@ export default function Page() {
           </ViewTransition>
           <div className="absolute inset-0 transition-colors duration-300 hover:bg-black/30" />
           <div className="absolute bottom-0 w-full bg-black/50 p-2 text-center font-bold text-2xl text-white md:bottom-20 md:p-8 md:text-4xl">
-            <ViewTransition name={`${hobby.id}-title`}>
+            <ViewTransition
+              name={`${hobby.id}-title`}
+              share="text-morph"
+              default="none"
+            >
               <h2>{convertToPascalCase(hobby.id)}</h2>
             </ViewTransition>
           </div>
